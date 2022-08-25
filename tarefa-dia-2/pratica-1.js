e02e1f6116eca0ef4e9995b125afbb8d3362250a
Márcio <marciomvpm@gmail.com>
2022-08-25 09:30:23 -0300

    Tarefa 2 Ituring Declarando Variaves

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,6 +1,16 @@
+//Tarefa 2 Declarando variáveis
+
+
+let nome
 nome = 'Márcio Vinícius Pereira Magela'
+
+let e_mail
 e_mail = 'marciomvpm@gmail.com'
+
+let profissao
 profissao = 'Analista de Dados'
+
+let idade
 idade = 31
 sonho = 'Australia'
 
