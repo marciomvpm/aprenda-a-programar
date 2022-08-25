f00b8abd12cf7d648947e638b3c2ecac54eff73c
Márcio <marciomvpm@gmail.com>
2022-08-25 10:00:19 -0300

    Tarefa 2 ituring concatenando variaveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,7 +10,8 @@ let idade
 idade = 31
 sonho = 'Australia'
 
-console.log(' Olá meu nome é ' + nome 
-+ ' se preicisar falar comigo me envia um e-mail ' 
-+ e_mail + ' eu sou ' 
-+ profissao + ' e meu sonho é um dia conhecer a ' + sonho)
+console.log(`Olá ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos.
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${sonho}.
+Confirma pra gente seus dados? Sua idade é ${idade}
+você é ${profissao} e o seu e-mail é ${e_mail}!
+Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
