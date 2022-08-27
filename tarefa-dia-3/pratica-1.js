4de703bbe9726d33f16342eae485a4fe48f74458
Márcio <marciomvpm@gmail.com>
2022-08-26 21:39:05 -0300

    Pratica 2 Tarefa Dia 3

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,8 @@
+///CONTADOR
+
+let n=1
+
+while (n<=9)
+{ console.log(n)
+    n++
+}
---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,14 @@
+//Tabuada de 9
+
+let i=1
+let n=10
+let multiplicador=9
+
+
+while (i<=10){
+let resultado = multiplicador * i
+let textoDeExibicao = `${multiplicador} X ${i} = ${resultado}`
+console.log(textoDeExibicao)
+i++
+}
+////
