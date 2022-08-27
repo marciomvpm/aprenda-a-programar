37d7ac15741ce365dd7c4fd25861cb18e9e1cc7c
Márcio <marciomvpm@gmail.com>
2022-08-26 22:09:50 -0300

    Tarefa dia 3

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,32 @@
+//Prática 4 (Opcional): É um número primo?
+//Números primos são aqueles que são divisíveis apenas pelo número 1 e por ele mesmo. 
+//Os números primos cumprem um papel essencial na área da criptografia, pois é por meio deles que conseguimos hoje criptografar informações.
+//Agora é com você! Crie um código no arquivo pratica-4.js que começando com a entrada 
+//de um número inteiro maior que zero, exiba Sim, o número ${numero} é primo! se este número for primo e Não, 
+//o número ${numero} não é primo! se ele não for um número primo. Como sugestão, pode colocar o nome da variável
+// que vai armazenar esse valor de entrada de numero. Veja abaixo alguns exemplos.
+//Alguns possíveis dados de entrada:
+//
+//17
+//2
+//7
+//8
+//5
+//23
+//15
+//1500450271
+//
+//Resultados exibidos no terminal para cada valor de entrada quando executar o código:
+//Sim, o número 17 é primo!
+//Sim, o número 2 é primo!
+//Sim, o número 7 é primo!
+//Não, o número 8 não é primo!
+//Sim, o número 5 é primo!
+//Sim, o número 23 é primo!
+//Não, o número 15 não é primo!
+//Sim, o número 1500450271 é primo!
+
+let numero = 1500450271
+
+
+
